faf666515f146387689d195c7263327c50e62622
AlejandrVilla <avillah@unsa.edu.pe>
2025-02-10 18:24:35 -0500

    jsconfig file and aliases in vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+      pages: "/src/pages",
+      home: "/src/pages/home",
+      layouts: "/src/layouts",
+      utils: "/src/utils",
+      assets: "/src/assets",
+      sass: "/src/sass"
+    }
+  }
 })
